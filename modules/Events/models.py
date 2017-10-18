cb13d5c74d8658a96859f47a6b889359e99b05c2
Pepe <joseotoncv@gmail.com>
2017-10-17 21:13:33 -0500

    Edité el modelo Event

--- modules/Events/models.py
+++ modules/Events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+	id_event = models.AutoField(primary_key=True,unique=True)
+	date_time = models.DateTimeField(blank=True, null=False)
+	lat = models.FloatField(max_digits=3,decimal_places=4)
+	lon = models.FloatField(max_digits=3,decimal_places=4)
+	address = models.CharField(max_length=250)
+	description = models.TextField()
