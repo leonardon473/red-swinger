c863811f12ca66c4c92e850375e074d2a23ee65a
Pepe <joseotoncv@gmail.com>
2017-10-17 21:49:25 -0500

    Agregué Comment para la tabla Post

--- modules/Posts/models.py
+++ modules/Posts/models.py
@@ -7,3 +7,11 @@ class Posts(models.Model):
 	id_user = models.ForeingKey()
 	text = models.TextField()
 	image = models.URLField()
+
+class Comment(models.Model)
+	id_comment = models.AutoField(primary_key=True,unique=True)
+	id_post = models.ForeingKey(Posts)
+	comment = models.CharField(max_length=150)
+
+
+
