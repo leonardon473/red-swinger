cb8c25e6497514206e2c52dd1f371d63e16b13b0
Pepe <joseotoncv@gmail.com>
2017-10-17 21:07:07 -0500

    Edité el modelo Post y el modelo Event

--- modules/Posts/models.py
+++ modules/Posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Posts(models.Model):
-	id_post = models.PrimaryKey(primary_key=True,unique=True)
+	id_post = models.AutoField(primary_key=True,unique=True)
 	id_user = models.ForeingKey()
 	text = models.TextField()
 	image = models.URLField()
