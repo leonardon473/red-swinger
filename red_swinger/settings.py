1ed2c67f44e123299c1248c36a0f5725ff0ed48e
Leonardo Ramírez <leonardon473@gmail.com>
2017-10-17 20:36:54 -0500

    Añadi las apps y hice migrate

--- red_swinger/settings.py
+++ red_swinger/settings.py
@@ -29,8 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+OUR_APPS = [
+    'modules.Events',
+    'modules.Posts',
+    'modules.Users'
+]
+'''
+THIRD_PACKAGE_APPS = [
+    'rest_framework',
+    'django_filters',
+]
+'''
+
+INSTALLED_APPS = DJANGO_APPS + OUR_APPS #+ THIRD_PACKAGE_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
