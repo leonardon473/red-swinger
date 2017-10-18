ad6145bb2276293b277450096e3b2261f9e74360
Pepe <joseotoncv@gmail.com>
2017-10-17 20:54:25 -0500

    Edité el modelo Post

--- modules/Posts/models.py
+++ modules/Posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+	id_post = models.PrimaryKey(primary_key=True,unique=True)
+	id_user = models.ForeingKey()
+	text = models.TextField()
+	image = models.URLField()
