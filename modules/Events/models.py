125ad5ce7dbff593cf4c271805d75a0205f99f7f
Leonardo Ramírez <leonardon473@gmail.com>
2017-10-19 15:23:11 -0500

    Estan listos todos los modelos

--- modules/Events/models.py
+++ modules/Events/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from modules.Users.models import User
 # Create your models here.
 
 class Event(models.Model):
 	#id_event = models.AutoField(primary_key=True,unique=True)
+	id_user = models.ForeignKey(User)   
 	date_time = models.DateTimeField(blank=True, null=False)
-	lat = models.FloatField(max_digits=3,decimal_places=4)
-	lon = models.FloatField(max_digits=3,decimal_places=4)
+	lat = models.FloatField()
+	lon = models.FloatField()
 	street = models.CharField(max_length=50)
 	number_ext = models.CharField(max_length=10)
 	number_int = models.CharField(max_length=10)
@@ -18,7 +19,7 @@ class Event(models.Model):
 
 class Comment(models.Model):
 	#id_comment = models.AutoField(primary_key=True,unique=True)
-	id_event = models.ForeingKey(Event)
+	id_event = models.ForeignKey(Event)
 	comment = models.CharField(max_length=150)
 
 '''
--- modules/Posts/models.py
+++ modules/Posts/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-
+from modules.Users.models import User
 # Create your models here.
 
-class Comment(models.Model)
-	#id_comment = models.AutoField(primary_key=True,unique=True)
+class Comment(models.Model):
+	id_user = models.ForeignKey(User)   
+	id_comment = models.AutoField(primary_key=True,unique=True)
 	comment = models.CharField(max_length=150)
 
 
 class Posts(models.Model):
 	#id_post = models.AutoField(primary_key=True,unique=True)
-	id_comment = models.ForeingKey(Comment)
+	id_comment = models.ForeignKey(Comment)
 	text = models.TextField()
 	image = models.URLField()
 
