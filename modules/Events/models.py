6f1918d5a2b3b51d59b59f202ec00b1773e8b51c
Pepe <joseotoncv@gmail.com>
2017-10-18 20:17:32 -0500

    Modelos finales de Event, Comment y Post, están comentadas Profille,Cover (fotos), así como Country y Country_state

--- modules/Events/models.py
+++ modules/Events/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Comment(models.Model)
-	id_comment = models.AutoField(primary_key=True,unique=True)
-	id_post = models.ForeingKey(Event)
-	comment = models.CharField(max_length=150)
-
-
 class Event(models.Model):
 	id_event = models.AutoField(primary_key=True,unique=True)
 	date_time = models.DateTimeField(blank=True, null=False)
@@ -21,3 +15,36 @@ class Event(models.Model):
 	country = models.CharField(max_length=50)
 	description = models.TextField()
 
+
+class Comment(models.Model):
+	id_comment = models.AutoField(primary_key=True,unique=True)
+	id_event = models.ForeingKey(Event)
+	comment = models.CharField(max_length=150)
+
+'''
+#Country and country_state
+
+class Country(models.Model):
+	id_country = models.AutoField(primary_key=True,unique=True)
+	name_country = models.CharField(max_length=100)
+
+#Country_state
+
+class Country_state(models.Model):
+	id_country_state = models.AutoField(primary_key=True,unique=True)
+	id_country = models.ForeingKey(Country)
+	state = models.CharField(max_length=100)
+
+
+#Profile Photo
+class Profile_photo(models.Model):
+	id_profile_photo = models.AutoField(primary_key=True,unique=True)
+	profile_photo = models.URLField()
+
+
+
+#Cover Photo
+class Cover_photo(models.Model):
+	id_cover_photo = models.AutoField(primary_key=True,unique=True)
+	cover_photo = models.URLField()
+'''
